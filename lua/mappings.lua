fcb287dd3934b4ff7727ddf4c300c9336b22e360
Yi Gu <yi.gu@reaktor.com>
2025-08-24 18:03:31 +0900

    Replace the leading colon with <cmd> in keymaps.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,26 +10,25 @@ map("!", "<C-h>", "<BS>")
 map({ "i" }, "<C-h>", "<Left>")
 
 local abbrev = vim.cmd.abbrev
-abbrev("w!!", ":w !sudo -A tee %")
+abbrev("w!!", "<cmd>w !sudo -A tee %<CR>")
 
 map("n", ";", ":", { desc = "enter command mode", nowait = true })
 
-map("n", "<leader>gs", ":Git<CR>", { desc = "Toggle git status"})
-map("n", "<leader>gb", ":Git blame<CR>", { desc = "Toggle git blame"})
+map("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Toggle git status"})
+map("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Toggle git blame"})
 
-map("n", "<S-Left>",  ":vertical resize -5<CR>", { desc = "Decrease window width." })
-map("n", "<S-Right>", ":vertical resize +5<CR>", { desc = "Increase window height." })
-map("n", "<S-Up>",    ":resize +5<CR>",          { desc = "Increase window height." })
-map("n", "<S-Down>",  ":resize -5<CR>",          { desc = "Decrease window height." })
-
+map("n", "<S-Left>",  "<cmd>vertical resize -5<CR>", { desc = "Decrease window width." })
+map("n", "<S-Right>", "<cmd>vertical resize +5<CR>", { desc = "Increase window height." })
+map("n", "<S-Up>",    "<cmd>resize +5<CR>",          { desc = "Increase window height." })
+map("n", "<S-Down>",  "<cmd>resize -5<CR>",          { desc = "Decrease window height." })
 
 map("n", "<C-s>", "<C-S> :%s/<C-R><C-W>//gc<Left><Left><Left>", { desc = "Search & replase current highlighted word" })
 
-map("n", "<leader>?", ":lua vim.diagnostic.open_float()<CR>", { desc = "Show diagnostic info.", noremap = true, silent = true })
+map("n", "<leader>?", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show diagnostic info.", noremap = true, silent = true })
 
-map("n", "<leader>mdpt", ":MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
-map("n", "<leader>mdp0", ":MarkdownPreviewStop<CR>", { desc = "Markdown Preview Stop" })
-map("n", "<leader>mdp1", ":MarkdownPreview<CR>", { desc = "Markdown Preview Start" })
+map("n", "<leader>mdpt", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
+map("n", "<leader>mdp0", "<cmd>MarkdownPreviewStop<CR>", { desc = "Markdown Preview Stop" })
+map("n", "<leader>mdp1", "<cmd>MarkdownPreview<CR>", { desc = "Markdown Preview Start" })
 
 map("v", "<C-s>", "y:%s/\\v<C-R>=escape(@\", '.*/\\[]^$+?\\|{}()')<CR>//gc<Left><Left><Left>", { desc = "Search & replase current highlighted word" })
 
@@ -44,11 +43,14 @@ end, { desc = "Indent + Format (with conform)", silent = true })
 
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
 
-map("n", "<leader>ff", ":FzfLua files<cr>", { desc = "Find files" })
-map("n", "<leader>fw", ":FzfLua live_grep<cr>", { desc = "Live grep" })
-map("n", "<leader>fb", ":FzfLua buffers<cr>", { desc = "Buffers" })
-map("n", "<leader>fh", ":FzfLua help_tags<cr>", { desc = "Help tags" })
+map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find files" })
+map("n", "<leader>fw", "<cmd>FzfLua live_grep<cr>", { desc = "Live grep" })
+map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
+map("n", "<leader>fh", "<cmd>FzfLua help_tags<cr>", { desc = "Help tags" })
 
 map("n", "[f", "<cmd>colder<CR>", { desc = "Quickfix stack older" })
 map("n", "]f", "<cmd>cnewer<CR>", { desc = "Quickfix stack newer" })
 
+map("n", "<leader>cc", "<cmd>CopilotChatToggle<CR>", { desc = "Toggle Copilot Chat" })
+map("n", "<leader>ce", "<cmd>CopilotChatExplain<CR>", { desc = "Explain Code" })
+map("v", "<leader>cc", "<cmd>CopilotChat<CR>", { desc = "Chat about selection" })
